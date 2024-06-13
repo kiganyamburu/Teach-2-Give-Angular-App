4dfc9253b0c28df5ba07ee7ec199689406f9599e
kiganyamburu <mburukiganya@gmail.com>
2024-06-13 18:23:38 +0300

    admin

--- techApp/src/app/app.routes.ts
+++ techApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AdminDashboardComponent } from './Components/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+];
